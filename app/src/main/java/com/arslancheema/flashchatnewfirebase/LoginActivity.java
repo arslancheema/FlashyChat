3eb1edcb4152cdc0aa1e4b46be67854dbda2f718
Arslangit config --global user.email Cheema <arslanch33ma@gmail.com>
2017-09-03 13:33:55 +0900

    Login / SignIn with Email completed

--- app/src/main/java/com/arslancheema/flashchatnewfirebase/LoginActivity.java
+++ app/src/main/java/com/arslancheema/flashchatnewfirebase/LoginActivity.java
@@ -2,14 +2,21 @@ package com.arslancheema.flashchatnewfirebase;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 
@@ -41,14 +48,13 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-
+        mAuth = FirebaseAuth.getInstance();
 
     }
 
     // Executed when Sign in button pressed
     public void signInExistingUser(View v)   {
-
-
+        attemptLogin();
     }
 
     // Executed when Register button pressed
@@ -59,7 +65,38 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void attemptLogin() {
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
 
+        if (email.equals("") || password.equals("")) return;
+
+        Toast.makeText(this,"Login in Progress ... ",Toast.LENGTH_SHORT);
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d("FlashChat", "SignIn with Email onComplete " + task.isSuccessful());
+                if (!task.isSuccessful()){
+                    Log.d("FlashChat", "Sign In Failed " + task.getException() );
+                    showErrorDialog("User Registration Failed");
+                } else {
+                    Log.d("FlashChat", "Sign In Succeeded ");
+                    Intent i = new Intent(LoginActivity.this, MainChatActivity.class);
+                    finish();
+                    startActivity(i);
+                }
+            }
+        });
+
+    }
+
+    private void showErrorDialog(String message){
+        // 1. Instantiate an AlertDialog.Builder with its constructor
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Oops")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok,null)
+                .show();
 
     }
 
